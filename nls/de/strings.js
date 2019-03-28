d3fd7d08ee8b522ebfb0748fd7a2beb9092f9226
pelatx <pelatx@gmail.com>
2019-03-28 11:36:22 +0100

    German translation fixes.

--- nls/de/strings.js
+++ nls/de/strings.js
@@ -5,7 +5,7 @@ define({
     DROP_AREA_DEST:     "Datei verlinken (Als Drop Bereich verwenden)",
     DOWNLOADER:         "Datei verlinken (Library herunterladen)",
     PREFERENCES:        "Link File Settings",
-    DROP_AREA_LABEL:    "Link File (Dateien löschen)",
+    DROP_AREA_LABEL:    "Link File (Dateien ablegen)",
 
     CDN_HEADER_TITLE: "Library herunterladen",
     CDN_HEADER_PLACEHOLDER: "Filtern ...",
@@ -22,7 +22,7 @@ define({
     CHECK_ALL_BUTTON: "Alle auswählen",
     UNCHECK_ALL_BUTTON: "Alle abwählen",
     FINISH_BUTTON: "Fertig",
-    HIDDEN_TOGGLE_LABEL: "Siehe versteckte",
+    HIDDEN_TOGGLE_LABEL: "Zeige verstecktes",
 
     PFSD_TITLE: "Dateien zum Projekt hinzufügen",
     PFSD_FILTER_PLACEHOLDER: "Filtern ...",
